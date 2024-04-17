716d875dc5264ed78efe493871eda51d049c6f5b
Dale Morris <dalemorr@iu.edu>
2024-04-16 23:30:13 -0400

    Added Node struct and methods

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,131 @@
-#include <fstream>
+#include <exception>
+#include <vector>
+#include <string>
 #include <iostream>
 
-struct Node {
-    int val;
-    Node** children;
-};
-
-struct Tree {
-    Node* head;
-};
-
-std::string get_file_name();
-
-Tree parse_data(std::ifstream& data_file);
-
 int main() {
-    std::cout << "Enter a file name: ";
-    std::string data_file_name = get_file_name();
+    std::cout << "Hello, World!" << std::endl;
+}
 
-    std::ifstream data_file (data_file_name);
-    Tree data;
-    if (data_file.is_open()) {
-        data = parse_data(data_file);
-    } else {
-        std::cout << "There was an issue opening the file" << std::endl;
-        return 1;
+struct Node {
+    int value;
+    int g_cost;
+    int h_cost;
+    int f_cost;
+
+    Node* parent;
+    std::vector<Node*>* adjacentNodes = new std::vector<Node*> {};
+
+    static inline auto* nodes = new std::vector<Node*> {};
+
+    Node (int value) {
+        this->value = value;
+
+        addNodeToNodes(this);
     }
 
-    return 0;
-}
+    ~Node()=default;
 
-std::string get_file_name() {
-    std::string file_name;
-    std::cin >> file_name;
-    return file_name;
-}
+    int getValue() {
+        return value;
+    }
 
-Tree parse_data(std::ifstream& data_file) {
-    Tree data = Tree{nullptr};
+    int getGCost() const {
+        return g_cost;
+    }
 
-    int num;
-    while (data_file >> num) {
-        if (num == -1) {
-            std::cout << std::endl;
+    void setGCost(int gCost) {
+        g_cost = gCost;
+    }
+
+    int getHCost() const {
+        return h_cost;
+    }
+
+    void setHCost(int hCost) {
+        h_cost = hCost;
+    }
+
+    int getFCost() const {
+        return f_cost;
+    }
+
+    void setFCost(int fCost) {
+        f_cost = fCost;
+    }
+
+    Node *getParent() const {
+        return parent;
+    }
+
+    void setParent(Node* parent) {
+        Node::parent = parent;
+    }
+
+    void addNodeToNodes(Node* node) {
+        nodes->emplace_back(node);
+    }
+
+    void addAdjacentNode(Node* node) {
+        for (auto & adjacentNode : *adjacentNodes) {
+            if (adjacentNode==node || adjacentNode->getValue() == node->getValue()) {
+                return;
+            }
+        }
+
+        adjacentNodes->emplace_back(node);
+    }
+
+    void addAdjacentByValue(int value){
+        if (getNode(value) == nullptr) {
+            addAdjacentNode(new Node(value));
         } else {
-            std::cout << num << " ";
+            addAdjacentNode(getNode(value));
         }
     }
 
-    return data;
-}
+    std::vector<Node*>* getAdjacentNodes() {
+        return adjacentNodes;
+    }
+
+    static std::string printNodes(std::vector<Node*>* nodes) {
+        std::string x = "";
+        for (Node* node : *nodes) {
+            x += node->getValue();
+            x += " ";
+        }
+
+        return x;
+    }
+
+    static Node* getNode(int value) {
+        for (int i = 0; i < nodes->size(); i++) {
+            if (nodes->at(i)->getValue() == value) {
+                return nodes->at(i);
+            }
+        }
+
+        return nullptr;
+    }
+
+    static std::vector<Node*>* getNodes() {
+        return nodes;
+    }
+
+    static void clearNodes() {
+        nodes = new std::vector<Node*> {};
+    }
+
+    static std::string NodesToString() {
+        std::string val;
+        val += "{";
+        for (int i = 0; i < nodes->size(); i++) {
+            val += std::to_string(nodes->at(i)->getValue());
+            if (i != nodes->size()-1) {
+                val += " ";
+            }
+        }
+        val += "}";
+        return val;
+    }
+};
