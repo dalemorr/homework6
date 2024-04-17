5853ed29d81a5b03a8a8117c5c04910a545741f2
Dale Morris <dalemorr@iu.edu>
2024-04-16 23:58:46 -0400

    Added main function

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,6 @@
 #include <string>
 #include <vector>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-}
-
 struct Node {
     int value;
     int g_cost;
@@ -268,3 +264,75 @@ struct Maze {
         return substr_list;
     }
 };
+
+int main() {
+    std::string File = "data/maze.txt";
+
+    Maze::ingestNodesFromFile(File);
+    std::vector<Node*>* path = Maze::A_Star(Node::getNode(1), Node::getNode(100));
+
+    std::cout << "File: " << File << std::endl;
+    std::cout << "Path:" << std::endl;
+
+    for (int i = 0; i < path->size(); i++) {
+        std::cout << path->at(i)->getValue();
+        if(i != path->size()-1) {
+            std::cout << "->";
+        }
+    }
+    std::cout << std::endl;
+
+    std::cout << "Path Cost: " << path->at(path->size()-1)->getFCost() << std::endl;
+
+    Node::clearNodes();
+
+    //test-maze
+
+    Node::clearNodes();
+
+    std::string File2 = "data/test-maze.txt";
+
+    Maze::ingestNodesFromFile(File2);
+
+    std::vector<Node*>* path2 = Maze::A_Star(Node::getNode(1), Node::getNode(6));
+
+    std::cout << "\nFile:";
+
+    std::cout << "\n" <<  File2 << ":\n";
+
+    std::cout << "Path:\n";
+
+    for(int i = 0; i < path2->size(); i++){
+        std::cout << path2->at(i)->getValue();
+        if(i != path2->size()-1)
+            std::cout << "->";
+    }
+    std::cout << "\nPath Cost: " << path2->at(path2->size()-1)->getFCost();
+
+    std::cout << '\n';
+
+    // test-maze2
+
+    Node::clearNodes();
+
+    std::string File3 = "data/test-maze2.txt";
+
+    Maze::ingestNodesFromFile(File3);
+
+    std::vector<Node*>* path3 = Maze::A_Star(Node::getNode(1), Node::getNode(7));
+
+    std::cout << "\nFile:";
+
+    std::cout << "\n" << File3 << ":\n";
+
+    std::cout << "Path:\n";
+
+    for(int i = 0; i < path3->size(); i++){
+        std::cout << path3->at(i)->getValue();
+        if(i != path3->size()-1)
+            std::cout << "->";
+    }
+    std::cout << "\nPath Cost: " << path3->at(path3->size()-1)->getFCost();
+
+    std::cout << '\n';
+}
